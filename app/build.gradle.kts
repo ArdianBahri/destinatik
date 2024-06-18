1fa147a2992729844a9e7843537d867f40cdadc3
lexynotfound <raihanardila@gmail.com>
2024-06-18 07:26:54 +0700

    change url api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         versionName = "1.0"
 
 
-//        buildConfigField("String", "BASE_URL", "\"https://api-service-backend-tpercgplna-uc.a.run.app/api/\"")
-        buildConfigField("String", "BASE_URL", "\"https://trial-capstone-426318.et.r.appspot.com/\"")
+        buildConfigField("String", "BASE_URL", "\"https://api-service-backend-tpercgplna-uc.a.run.app/api/\"")
+//        buildConfigField("String", "BASE_URL", "\"https://trial-capstone-426318.et.r.appspot.com/\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
